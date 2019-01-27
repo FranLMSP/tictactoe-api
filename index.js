db0b4ba5c91340141357353162d8488b1562e9dd
Franco Colmenarez <francoacg1@gmail.com>
2019-01-27 12:05:03 -0400

    Getting messages

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public' + '/index.html');
-});
+  res.sendFile(__dirname + '/public' + '/index.html')
+})
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a user connected')
+
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', function(msg) {
+    console.log('New message: ' + msg)
+  })
+
+})
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
