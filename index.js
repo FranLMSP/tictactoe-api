4606346aa373a84d351cdc24575e8e1ab04badeb
Franco Colmenarez <francoacg1@gmail.com>
2019-01-27 12:22:23 -0400

    Appending new messages to the body

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ io.on('connection', socket => {
   })
 
   socket.on('chat message', msg => {
-    console.log('New message: ' + msg)
+    io.emit('new message', msg)
   })
 
 })
 
-http.listen(3000, () => {
+http.listen(3000, () =>   {
   console.log('listening on *:3000')
 })
