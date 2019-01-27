de4bab37ac63c6aa7fdb582b357d22f3f6b2747f
Franco Colmenarez <francoacg1@gmail.com>
2019-01-27 12:09:39 -0400

    Replacing functions by arrow functions

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public' + '/index.html')
 })
 
-io.on('connection', function(socket){
+io.on('connection', socket => {
   console.log('a user connected')
 
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 
-  socket.on('chat message', function(msg) {
+  socket.on('chat message', msg => {
     console.log('New message: ' + msg)
   })
 
 })
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000')
 })
